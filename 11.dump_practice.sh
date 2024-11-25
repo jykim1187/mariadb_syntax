0d57fe5d6acdb62530edc0bad3cf10fd0d9cb229
jykim1187 <rifkehtm862@gmail.com>
2024-11-25 10:36:56 +0900

    11/25/practice

---
+++ 11.dump_practice.sh
@@ -0,0 +1,29 @@
+# 덤프파일 생성 : dumpfile.sql이라는 이름의 덤프파일 생성
+mysqldump -u root -p board > dumpfile.sql
+
+# 한글 깨질 때
+mysqldump -u root -p -r board dumpfile.sql
+
+# 덤프파일 적용(복원)
+# <가 예약어로 인식되어, window에서는 적용이 안될 경우, git bash 터미널 창을 활용한다.
+mysql -u root -p board < dumpfile.sql
+
+# 덤프파일을 github에 업로드
+
+# 리눅스에서 mariadb설치
+sudo apt-get install mariadb=server
+
+# mariadb 서버 실행
+sudo systemctl start mariadb
+
+# mariaDB 접속 :1234
+mariadb -u root -p
+create database board;
+
+# git 설치
+sudo apt install git
+
+# git에서 repository clone
+git clone 레포지토리 주소
+
+#dump파일 복원
---
+++ dumpfile.sql
@@ -0,0 +1,182 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.5.2-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: board
+-- ------------------------------------------------------
+-- Server version	11.5.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `created_time` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES
+(1,'진영@naver.com','1234','김진영','2024-11-22 10:20:14'),
+(2,'hans1@naver.com',NULL,'hans','2024-11-22 11:28:06'),
+(3,'hans2@naver.com',NULL,'hansi','2024-11-22 11:28:18'),
+(4,'hans3@naver.com',NULL,'hansu','2024-11-22 11:28:19'),
+(5,'july1@naver.com',NULL,'july','2024-11-22 11:28:20'),
+(6,'july2@naver.com',NULL,'jully','2024-11-22 11:28:21');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `author_address`
+--
+
+DROP TABLE IF EXISTS `author_address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author_address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `author_id` bigint(20) NOT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `created_time` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `author_address_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author_address`
+--
+
+LOCK TABLES `author_address` WRITE;
+/*!40000 ALTER TABLE `author_address` DISABLE KEYS */;
+INSERT INTO `author_address` VALUES
+(1,2,'korea','incheon','san-ro','2024-11-22 11:28:34'),
+(2,2,'korea','incheon','san-ro','2024-11-22 11:28:35'),
+(3,4,NULL,NULL,NULL,'2024-11-22 11:28:39'),
+(4,5,NULL,NULL,NULL,'2024-11-22 11:28:40');
+/*!40000 ALTER TABLE `author_address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `author_post`
+--
+
+DROP TABLE IF EXISTS `author_post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author_post` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `post_id` bigint(20) NOT NULL,
+  `author_id` bigint(20) NOT NULL,
+  `created_time` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `post_id` (`post_id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `author_post_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`),
+  CONSTRAINT `author_post_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author_post`
+--
+
+LOCK TABLES `author_post` WRITE;
+/*!40000 ALTER TABLE `author_post` DISABLE KEYS */;
+INSERT INTO `author_post` VALUES
+(1,1,1,'2024-11-22 11:31:15'),
+(2,1,3,'2024-11-22 11:37:59');
+/*!40000 ALTER TABLE `author_post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `author_post2`
+--
+
+DROP TABLE IF EXISTS `author_post2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author_post2` (
+  `author_id` bigint(20) NOT NULL,
+  `post_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`author_id`,`post_id`),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `author_post2_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`),
+  CONSTRAINT `author_post2_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author_post2`
+--
+
+LOCK TABLES `author_post2` WRITE;
+/*!40000 ALTER TABLE `author_post2` DISABLE KEYS */;
+/*!40000 ALTER TABLE `author_post2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `contents` varchar(255) DEFAULT NULL,
+  `created_time` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES
+(1,'반갑습니다','첫 회원입니다!','2024-11-22 10:22:29'),
+(2,'hello,python','hi,we are python!','2024-11-22 11:26:53'),
+(3,'hello,java','hi,i`m java!','2024-11-22 11:29:15');
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2024-11-25 10:03:49
